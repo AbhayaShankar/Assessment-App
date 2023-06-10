ec37b983f9cdfddccf6bb507e98a074a3d0c300e
Abhaya <shankarabhaya9@gmail.com>
2023-06-11 03:02:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable no-unused-vars */
 import RegistrationForm from "./components/Form";
 import Questions from "./components/Questions";
-import Summary from "./components/Summary";
 import { useSelector, useDispatch } from "react-redux";
 import blob1 from "./assets/blob1.png";
 import blob2 from "./assets/blob2.png";
-import {
-  registerUser,
-  startAssessment,
-  selectAnswer,
-  submitAssessment,
-} from "./actions/action";
+import blob3 from "./assets/blob3.png";
+import blob4 from "./assets/blob4.png";
+import Navigation from "./components/Navigation";
 
 function App() {
   const user = useSelector((state) => state.user);
@@ -27,6 +23,9 @@ function App() {
       )}
       <img className="blob1" src={blob1} alt="blob1" />
       <img className="blob2" src={blob2} alt="blob2" />
+      <img className="blob3" src={blob3} alt="blob3" />
+      <img className="blob4" src={blob4} alt="blob4" />
+      <Navigation />
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,18 @@
-// Nvaigation Panel for quick view of Questions.
+import { useState } from "react";
+import Assessment from "../assessment.json";
 
+// Nvaigation Panel for quick view of Questions.
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  return (
+    <div>
+      Abhaya
+      {Assessment.questions.map((quest, index) => {
+        <>{index}</>;
+      })}
+    </div>
+  );
 };
 
 export default Navigation;
